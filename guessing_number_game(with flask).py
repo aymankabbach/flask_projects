c994917538607463ca2586e451cc768cb46f366a
ayman <kabbachayman95@gmail.com>
2022-12-23 19:58:26 +0100

    the program randomly choose a number

--- guessing_number_game(with flask).py
+++ guessing_number_game(with flask).py
@@ -1,9 +1,9 @@
 from flask import Flask
+import random 
 app=Flask(__name__)
-
+random_number=random.randint(1,100)
 @app.route('/')
 def home_page():
-    return "my flask project"
-
+    return str(random_number)
 if __name__=="__main__":
     app.run(debug=True)
