fb5f80fb7372e59478f1608dbda318f230e14302
ayman <kabbachayman95@gmail.com>
2022-12-24 17:25:44 +0100

    write game instructions

--- guessing_number_game/guessing_number_game(with flask).py
+++ guessing_number_game/guessing_number_game(with flask).py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 import random 
 app=Flask(__name__)
 random_number=random.randint(1,100)
+attempts=5
 @app.route('/')
 def home_page():
-    return str(random_number)
+    return render_template("home_page.html",attempts=str(attempts))
 if __name__=="__main__":
     app.run(debug=True)
