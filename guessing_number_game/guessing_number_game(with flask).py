acee0c0f7f486f3ce7531d6c8f55510cae5b57bb
ayman <kabbachayman95@gmail.com>
2022-12-25 16:25:04 +0100

    creating a form for the user

--- guessing_number_game/guessing_number_game(with flask).py
+++ guessing_number_game/guessing_number_game(with flask).py
@@ -1,10 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import random 
 app=Flask(__name__)
 random_number=random.randint(1,100)
 attempts=5
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def home_page():
+    if request.method == 'POST':
+        return request.form['userguess']
     return render_template("home_page.html",attempts=str(attempts))
 if __name__=="__main__":
     app.run(debug=True)
