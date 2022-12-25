3356608f001ada3dc66fb26a334ef68d56a91616
ayman <kabbachayman95@gmail.com>
2022-12-25 17:30:13 +0100

    check and compare the user's number

--- guessing_number_game/guessing_number_game(with flask).py
+++ guessing_number_game/guessing_number_game(with flask).py
@@ -3,10 +3,24 @@ import random
 app=Flask(__name__)
 random_number=random.randint(1,100)
 attempts=5
+message=""
+def check(user_number):
+    global attempts
+    if user_number<random_number:
+        attempts=attempts-1
+        message="too low"
+        return message
+    elif user_number>random_number:
+        attempts=attempts-1
+        message="too high"
+        return message
+    else:
+        message="you get it right"
+        return message
 @app.route('/', methods=['GET','POST'])
 def home_page():
     if request.method == 'POST':
-        return request.form['userguess']
-    return render_template("home_page.html",attempts=str(attempts))
+        return render_template("home_page.html",attempts=str(attempts),message=check(int(request.form['userguess'])))
+    return render_template("home_page.html",attempts=str(attempts), message=message)
 if __name__=="__main__":
     app.run(debug=True)
