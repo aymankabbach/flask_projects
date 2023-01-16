641242e77b60f40f59b8aed01841aed48c6bd725
ayman <kabbachayman95@gmail.com>
2023-01-16 22:55:33 +0100

    create home page

---
+++ first_equation_solver/first_equation_solver.py
@@ -0,0 +1,9 @@
+from flask import Flask,render_template,request
+import random 
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home_page.html")
+if __name__=="__main__":
+    app.run(debug=True)
