487c560166f605206e9292d4fbf5b5d0a324f912
ayman <kabbachayman95@gmail.com>
2023-01-16 23:23:21 +0100

    send the solution to the user

--- first_equation_solver/first_equation_solver.py
+++ first_equation_solver/first_equation_solver.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request
 app=Flask(__name__)
+solution=""
 def solve_equation(a,b):
     if a==0:
         if b==0:
@@ -11,9 +12,13 @@ def solve_equation(a,b):
         return x
 @app.route('/',methods=['GET','POST'])
 def home():
+    global solution
     if request.method == 'POST':
         solution=solve_equation(int(request.form['value a']),int(request.form['value b']))
-        print(solution)
-    return render_template("home_page.html")
+        if type(solution)!=str:
+            return render_template("home_page.html", solution=str(solution))
+        else:
+            return render_template("home_page.html", solution=solution)
+    return render_template("home_page.html", solution=solution)
 if __name__=="__main__":
     app.run(debug=True)
