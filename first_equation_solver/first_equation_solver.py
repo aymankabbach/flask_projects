8c85813b69e60c109126471d26a36d19e546de0a
ayman <kabbachayman95@gmail.com>
2023-01-16 23:16:40 +0100

    get result to the equation

--- first_equation_solver/first_equation_solver.py
+++ first_equation_solver/first_equation_solver.py
@@ -1,8 +1,19 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app=Flask(__name__)
-
-@app.route('/')
+def solve_equation(a,b):
+    if a==0:
+        if b==0:
+            return "all numbers are solution to the equation"
+        else:
+            return "no solution to this equation"
+    else:
+        x=-b/a
+        return x
+@app.route('/',methods=['GET','POST'])
 def home():
+    if request.method == 'POST':
+        solution=solve_equation(int(request.form['value a']),int(request.form['value b']))
+        print(solution)
     return render_template("home_page.html")
 if __name__=="__main__":
     app.run(debug=True)
